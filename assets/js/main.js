eccf765c053b04b32e44dd5b69aee9927a76f90d
Yannick L <lef.yannick@gmail.com>
2019-04-19 15:44:31 +0200

    commit

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+$('#hide').click(function () {
+$('p').hide(2000);
+});
+});
+
+$(document).ready(function () {
+
+$('#show').click(function () {
+$('p').show(2000);
+});
+});
